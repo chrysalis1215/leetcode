ee09705ec83e7640230b5d9948b0c4399de613c4
jingya <jingya_z@foxmail.com>
2025-06-01 11:34:20 -0700

    feat: binary search 35

--- editor/cn/[35]Search Insert Position.py
+++ editor/cn/[35]Search Insert Position.py
@@ -1,5 +1,20 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+
+        return left
+
         
 # leetcode submit region end(Prohibit modification and deletion)
