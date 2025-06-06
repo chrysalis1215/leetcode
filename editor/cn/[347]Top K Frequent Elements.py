8379d563f2045ea5c018d91c82d18d17fc2136d6
jingya <jingya_z@foxmail.com>
2025-06-05 23:40:52 -0700

    feat: hashmap 347

--- editor/cn/[347]Top K Frequent Elements.py
+++ editor/cn/[347]Top K Frequent Elements.py
@@ -1,5 +1,10 @@
+from collections import Counter, defaultdict
+
 # leetcode submit region begin(Prohibit modification and deletion)
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+        count = Counter(nums)
+        # Sort by frequency descending
+        return [num for num, freq in sorted(count.items(), key=lambda x: x[1], reverse=True)[:k]]
 # leetcode submit region end(Prohibit modification and deletion)
