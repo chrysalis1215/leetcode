95db1e8b80ed2a0e05e8d5a9a541fc54e00258b9
jingya <jingya_z@foxmail.com>
2025-06-25 16:31:43 -0700

    feat: recursion 206

--- editor/cn/[206]Reverse Linked List.py
+++ editor/cn/[206]Reverse Linked List.py
@@ -1,19 +1,27 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = head
+        if not head or not head.next:
+            return head
 
-        if node.
-        while node:
-            self.resverse(node.next)node
+        return self.resverse(None, head)
 
-    def resverse(self, pre:[ListNode], node:[ListNode]):
-        pre.next = node
+
+    def resverse(self, pre:[ListNode], cur:Optional[ListNode]):
+        temp = cur.next
+        cur.next = pre
+        if not temp:
+            return cur
+
+        return  self.resverse(cur, temp)
 
 
         
