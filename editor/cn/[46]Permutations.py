39b12b76c15eef3deb56fa062dcae47a77828a81
jingya <jingya_z@foxmail.com>
2025-06-26 00:15:41 -0700

    feat: recursion  206 docs

---
+++ editor/cn/[46]Permutations.py
@@ -0,0 +1,9 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        for i, num in enumerate(nums):
+
+
+        
+# leetcode submit region end(Prohibit modification and deletion)
