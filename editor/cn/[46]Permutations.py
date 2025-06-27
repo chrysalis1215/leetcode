24be12fbd3cfc73a4751dd33c780320ebe6b2fa3
jingya <jingya_z@foxmail.com>
2025-06-27 00:30:38 -0700

    feat: backtracking 46

--- editor/cn/[46]Permutations.py
+++ editor/cn/[46]Permutations.py
@@ -1,8 +1,23 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
 
-        for i, num in enumerate(nums):
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for i,  num in enumerate(nums):
+                if not used[i]:
+                    used[i] = True
+                    path.append(num)
+                    backtrack(path, used)
+                    path.pop()
+                    used[i] = False
+
+        backtrack([], [False] * len(nums))
+        return res
 
 
         
